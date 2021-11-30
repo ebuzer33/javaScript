7ee45986b894ef87d661f48b992cfb87e399a3c7
ebuzer <ebuzer.tutuk@gmail.com>
2021-11-30 22:57:14 +0200

    console and Data Types

---
+++ index.js
@@ -0,0 +1,78 @@
+console.log ("Hello world !")
+
+var userName="Gamze"
+
+console.log(userName)
+
+/*
+var, let, const
+*/
+
+userName ="Ebuzer"
+
+console.log(userName)
+
+// let -> mutable
+
+let mail="abd@gmail.com"
+
+console.log(mail)
+
+mail = "xyz@gmail.com"
+
+// const ->immutable
+
+console.log(mail)
+
+const email="esd@gmail.com"
+
+//email ="azs@gmail.com"
+
+let uName;
+
+//const secondName;
+
+let a=3;
+let b=8;
+
+console.log(a);
+let c=a;
+a=b;
+b=c;
+
+console.log(a);
+console.log(b);
+
+
+/// Data Types ///
+
+/* Primitive  Type
+
+1. string " ", ' '
+2. number
+3. boolean
+4. undefined
+5. null
+*/
+
+let name_1= "Victor";
+
+console.log(typeof name_1);
+
+console.log(typeof 29);
+
+console.log(typeof 3.14);
+
+console.log(typeof true)
+
+let phoneNumber;
+console.log(typeof phoneNumber)
+
+let email_1=null;
+console.log(typeof email_1)
+
+
+
+
+
+
