4cd46740ceaed8a9d0cb414afe2e036417830dcc
ebuzer <ebuzer.tutuk@gmail.com>
2021-12-07 20:26:24 +0200

    operators

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log ("Hello world !")
 
-var userName="Gamze"
+var userName="Maria"
 
 console.log(userName)
 
@@ -8,7 +8,7 @@ console.log(userName)
 var, let, const
 */
 
-userName ="Ebuzer"
+userName ="Nikos"
 
 console.log(userName)
 
@@ -71,6 +71,274 @@ console.log(typeof phoneNumber)
 let email_1=null;
 console.log(typeof email_1)
 
+
+/* Basic Operators  */
+
+// 1. Aritmetik
+
+let currentYear=2021;
+let age= currentYear -1988;
+console.log("age= ",age)
+
+let x=3;
+let y=9;
+let total=x+y;
+console.log(total)
+
+let xy=3*6;
+console.log(xy)
+
+let as=12/6;
+console.log(as)
+
+let exponent=2**3;
+console.log(exponent)
+
+let mod=10%3;
+console.log(mod)
+
+// 2.Increment decrement
+
+let ab=3;
+let xz=--ab;console.log(ab,xz)
+
+let abc=3;
+let zyx=abc--;
+console.log(zyx,abc)
+
+// String concetanation
+
+const firstName= "Jess"
+const lastName="Pikman"
+
+console.log(firstName +" "+ lastName)
+
+// Assignment Operator
+
+let value = true;
+let x1 = 2;
+
+x1=x1+5;
+console.log("x1 = "+x1);
+
+x1 = x1+5;
+console.log("x1 = "+x1);
+
+x1 += 5;
+console.log("x1 = "+x1);
+
+let y1= 10;
+y1/2;
+console.log("y1 =", y1);
+
+y1*4;
+console.log("y1 =", y1);
+
+x1 +=y1;
+console.log("x1 =",x1)
+
+// Comparison Operators
+
+let ageJess = 50;
+let ageMaria =30;
+
+// This gives boolean value
+
+console.log(ageJess > ageMaria);
+console.log(ageJess < ageMaria);
+console.log(ageJess==50);
+
+console.log(ageMaria != 30);
+
+console.log("1", ageJess =="50"); // loose comparison
+
+console.log("2", ageJess === "50"); // strict comparison
+
+console.log("2", ageJess !== "50");
+
+
+/* Template literal */
+
+let name1 ="Jack";
+let surname= "Aspro";
+let work= "Amazon";
+
+let sentence= "He is "+ name1 + " " + surname +" and He is founder of "+ work + ".";
+console.log(sentence);
+
+let betterSentence= `He is ${name1} ${surname}, and he is founder of ${work}.`;
+console.log(betterSentence);
+
+console.log("I want to write this \n\
+on new line");
+
+console.log(`I want to write this \n\
+on new line`);
+
+console.log("For github desktop.");
+
+/* Conditionals */
+
+const currentAge = 15;
+
+// If the statement within the paranthesis is true, the code within the if block will execute.
+
+if(currentAge >=18) 
+{
+    console.log("You can drive legally");
+}
+else
+{
+    console.log("You are not allowed to drive");
+}
+
+const birthYear=2001;
+
+let generation;
+
+if(birthYear>=2000){
+    generation="Z";
+    console.log(generation);
+}
+else if(birthYear<2000)
+{
+    generation="Y";
+    console.log(generation);
+}
+else
+{
+    generation="A";
+    console.log(generation);
+}
+
+
+let grade =80
+
+if(grade>=80)
+{
+    let lateNote="AA"
+    console.log("Late note= "+lateNote)
+}
+else if(grade <=80)
+{
+  lateNote="BB"
+  console.log("Late note= "+lateNote)
+}
+else if(grade <=60)
+{
+  lateNote="CC"
+  console.log("Late note= "+lateNote)
+}
+else if(grade <=30)
+{
+  lateNote="DD"
+  console.log("Late note= "+lateNote)
+}
+else
+{
+    lateNote="ZZ"
+    console.log("Late note= "+lateNote) 
+}
+
+// Type Conversion and Type Coercion
+
+// Type conversion -> Manually convert type
+
+console.log(typeof "18");
+
+console.log(typeof Number("18"))
+
+console.log(typeof String(18))
+
+console.log(Number("Maria")) // Not a Number
+
+console.log(Number("3,14")) // Not a Number
+
+console.log(typeof NaN)
+
+// Type coersion -> JS converts value behind the scenes
+
+let num ="5";
+let mult= num*2;
+console.log("mult =", mult)
+
+let divide =10 /num;
+console.log("divide =", divide)
+
+let minus = num-1;
+console.log("minus =", minus);
+
+let add = num+5;
+console.log("add =",add) // concatenate the values
+
+let add2 =Number(num)+5;
+console.log("add2 = ",add2)
+
+let h = "1";
+h+=1; // string 11 
+h-=1; // number 10
+console.log(h)
+
+// Truthy and falsy values
+
+/*
+1. 0
+2. ""
+3. undefined
+4. null
+5. NaN
+
+*/
+
+let cash = 0;
+if (cash)
+{
+    console.log("find a job")
+}
+
+// If you write one line code u don't have to use curly braces
+
+if(cash == 0) console.log("No jobs found")
+
+/*
+&& (And)
+    true && true => true
+    true && false => false
+    false && false => false
+
+|| (or)
+    true || true => true
+    true || false => true
+    false || false => false
+
+! (not)
+!true => false
+!false=> true
+*/
+
+/*
+"AND" Operator
+
+    const a= "have license"  (true)
+    const b = "bigger than 18"  (true)
+
+    a && b = "can drive"   (true)
+    !a && b = "cannot drive" (false)
+    a && !b = "cannot drive"  (false)
+    !a && !b = "cannot drive "(false)
+
+*/
+
+/* 
+ * OR OPERATOR 
+    const x = bigger than 65 
+    const y = chronic disease 
+
+    x || y = risky (true)
+    !x || y = risky (true)
+    x || !y = risky (true)
+    !x || !y = not risky (false)
+*/
 
 
 
